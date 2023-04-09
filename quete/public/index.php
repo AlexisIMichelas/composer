093f1e4fbbdd476a67cb5278118d3c475282cae9
AMichelas <120266869+AMichelas@users.noreply.github.com>
2023-04-09 14:23:15 +0200

    ajout

---
+++ quete/public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$hello = new Quete\Src\Hello();
+echo $hello->talk(); 
+
+?>
---
+++ quete/src/Hello.php
@@ -0,0 +1,8 @@
+<?php
+namespace Quete\Src;
+class Hello {
+public function talk() {
+    return "Hello world!";
+}
+}
+?>
